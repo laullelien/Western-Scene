138471948df8fc581fb8b7f85c137dbab13c1bff
Aurelien <aurelien.delory@grenoble-inp.org>
2024-03-29 20:54:38 +0100

    Added interpolation

--- terrain.py
+++ terrain.py
@@ -1,11 +1,14 @@
 from core import Mesh
 from random import randint
 import numpy as np
+from time import time
 
 class Terrain(Mesh):
     def __init__(self, position, shader):
+        self.__init_perlin_noise(int(time()))
+
         index = list()
-        pos = np.array([np.array((i, j, randint(0, 10))) for i in range(100) for j in range(100)])
+        pos = np.array([np.array((i, j, self.__get_noise(i, j, 30, 1/10))) for i in range(100) for j in range(100)])
         norm = np.zeros(shape = (10000, 3))
 
         for i in range(99):
@@ -36,3 +39,26 @@ class Terrain(Mesh):
             norm[i] /= np.linalg.norm(norm[i])
         
         super().__init__(shader, attributes = dict(normal = norm,position = pos), index = index)
+    
+    def __init_perlin_noise(self, seed):
+        np.random.seed(seed)
+
+        self.perlin = np.array([[np.array(np.random.rand()) for i in range(100)] for j in range(100)])
+
+    def __get_noise(self, x, y, amplitude, freq):
+        x *= freq
+        coord_x = int(x) % 200
+        if (coord_x >= 100):
+            coord_x = 199 - coord_x
+
+        y *= freq
+        coord_y = int(y) % 200
+        if (coord_y > 100):
+            coord_y = 199 - coord_y
+
+        return amplitude * self.__interpolation(x - int(x), y - int(y), self.perlin[coord_x][coord_y], self.perlin[coord_x][coord_y + 1], self.perlin[coord_x + 1][coord_y], self.perlin[coord_x + 1][coord_y + 1])
+
+
+    def __interpolation(self, x, y, t, u, v, w):
+        return (w + t - u - v) * x * y + (v - t) * x + (u - t) * y + t
+
