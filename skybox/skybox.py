1ad43c76627bf6cbf3ff4c0e0b0d174c07ff7eb9
Thomas Serafin <Thomas.Serafin@grenoble-inp.org>
2024-04-02 15:08:37 +0200

    .vscoe

--- skybox/skybox.py
+++ skybox/skybox.py
@@ -1,6 +1,12 @@
 import OpenGL.GL as GL 
 
 #https://learnopengl.com/Advanced-OpenGL/Cubemaps
+# https://www.youtube.com/watch?v=ZwNDBOUmyLY  17:40
 
-def loadSkybox(list_image):
-    GL.glGenTextures(1, )
+def loadSkybox(list_image): 
+    texture = GL.glGenTextures(1)
+    GL.glBindTexture(GL_TEXTURE_CUBE_MAP, texture)
+
+    for i in range(len(list_image)):
+        #bla
+   
