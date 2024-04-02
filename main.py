10f94c48c3ea8f92f3a7ca8fe8d9886d125b699e
Thomas Serafin <Thomas.Serafin@grenoble-inp.org>
2024-04-02 14:37:00 +0200

    add skybox.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from terrain.terrain import Terrain
 
 def main():
     viewer = Viewer()
+    #load skybox
+    #TODO
+
+    #load files necessary for terrain generation
     terrainShader = Shader("terrain/terrain.vert", "terrain/terrain.frag")
+
+    #add terrain
     viewer.add(Terrain(1, terrainShader))
     viewer.run()
 
---
+++ skybox/skybox.py
@@ -0,0 +1,6 @@
+import OpenGL.GL as GL 
+
+#https://learnopengl.com/Advanced-OpenGL/Cubemaps
+
+def loadSkybox(list_image):
+    GL.glGenTextures(1, )
