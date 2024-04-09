1be65bda15d51cde12175fe71edf885c7e4223b8
Breno Morais <breno.da-silva-morais@grenoble-inp.org>
2024-04-09 15:08:23 +0200

    Start of tree generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from core import Viewer, Shader
 from terrain.terrain import Terrain
+from plant.plant import Tree
 
 def main():
     viewer = Viewer()
@@ -8,7 +9,11 @@ def main():
 
     #load files necessary for terrain generation
     terrainShader = Shader("terrain/terrain.vert", "terrain/terrain.frag")
-    viewer.add(Terrain(terrainShader))
+    #viewer.add(Terrain(terrainShader))
+
+    #load plants
+    treeShader = Shader("plant/plant.vert", "plant/plant.frag")
+    viewer.add(Tree(terrainShader))
     viewer.run()
 
 if __name__ == "__main__":
